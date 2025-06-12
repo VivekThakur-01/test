930c3474734ea8e6eae1e754f874c415c542207a
VivekThakur-01 <vivekthakur97976@gmail.com>
2025-06-12 19:51:06 +0530

    firstcommit

---
+++ arrays.js
@@ -0,0 +1,47 @@
+let myArray = [1,2,3,4,5]
+// console.log(myArray[1]);
+// console.log(myArray.push(7));
+
+
+// myArray.push(6)
+// console.log(myArray );
+
+// myArray.pop(3)
+// console.log(myArray );
+
+// myArray.unshift(9)
+// console.log(myArray);
+ 
+// myArray.shift(7)
+// console.log(myArray);
+
+// console.log(myArray.slice(1,3));
+
+
+// console.log(myArray.splice(3,5));
+
+// console.log(myArray);
+
+
+
+let arr1 = [1,2,3]
+let arr2 = [2,5,8]
+
+// console.log(arr1.concat(arr2));
+
+
+const newhero = [...arr1,...arr2]
+// console.log(newhero);
+
+const anotherarray = [1,2,3,[8,5,[7,8,[6,5]]]]
+const ekor = anotherarray.flat(Infinity)
+// console.log(ekor);
+
+
+console.log(Array.isArray ("vivek"));
+// console.log(Array.from("vivek"))
+
+
+console.log(Array.of("vuvek"));
+
+
